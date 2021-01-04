ef043ceefb58d8c7e472cf06057d001bd6472eac
Jivitesh13 <jivitesh13@yahoo.com>
2021-01-04 01:22:38 +0000

    Add files via upload

---
+++ MockSearchData.sql
@@ -0,0 +1,104 @@
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',19,GETDATE()-29)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',18,GETDATE()-28)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',17,GETDATE()-27)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',16,GETDATE()-26)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',15,GETDATE()-25)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',14,GETDATE()-24)
+GO
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',10,GETDATE()-23)
+	 
+GO
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',11,GETDATE()-22)
+	 
+GO
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (1,'Land Registry Search','www.infotrack.co.uk',5,GETDATE()-21)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',19,GETDATE()-29)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',18,GETDATE()-28)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',27,GETDATE()-25)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',13,GETDATE()-22)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',40,GETDATE()-18)
+GO
+
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',35,GETDATE()-13)
+GO
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',11,GETDATE()-11)
+	 
+GO
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',11,GETDATE()-10)
+	 
+GO
+
+INSERT INTO [dbo].[SearchHistory] ([SearchProviderTypeId] ,[SearchPhrase],
+		[UrlToSearchInResult],[Rank],[LastModifiedOn])
+     VALUES (2,'Land Registry Search','www.infotrack.co.uk',9,GETDATE()-5)
+GO
+
